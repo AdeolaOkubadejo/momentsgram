91bdabf58a3ee1ee1de30f638033736d293c192d
Adeola Okubadejo <101734397+AdeolaOkubadejo@users.noreply.github.com>
2025-10-16 14:12:07 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,5 +9,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    sourcemap: false // Disable sourcemaps to fix build errors
+  },
+  css: {
+    postcss: "./postcss.config.js" // Ensure Tailwind/Autoprefixer work
+  }
+});
 
